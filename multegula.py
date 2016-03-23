d36c71c37b462e7bee1514f17ce4127ec2b2f598
Garrett Miller <garrettmiller@gmail.com>
2016-03-23 15:16:48 -0400

    First commit of main driver script. Depends on presence of file UI.py with function runUI.

---
+++ multegula.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+###########################################################
+#Multegula - multegula.py						          #
+#Main Driver/Startup Script for Multegula                 #
+#Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He#
+###########################################################
+#TODO: place multegula UI file in UI.py, add function "runUI" to run it.
+
+from UI import * #Import our UI functions
+import subprocess #Needed for system calls
+
+#Start MessagePasser
+startweb = subprocess.Popen(['go', 'run', 'MessagePasser.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+
+#Start UI
+p1 = Process(target=runUI)
+p1.start()
+p1.join()
