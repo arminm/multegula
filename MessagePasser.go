ff0544e83a022e196a3afbc31a7d3f0f1db0e47f
Garrett Miller <garrettmiller@gmail.com>
2016-03-23 16:28:12 -0400

    Added MessagePasser.go with config reading example.

---
+++ MessagePasser.go
@@ -0,0 +1,26 @@
+////////////////////////////////////////////////////////////
+//Multegula - MessagePasser.go 
+//Main Driver/Startup Script for Multegula
+//Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He
+////////////////////////////////////////////////////////////
+
+import (
+    "encoding/json"
+    "os"
+    "fmt"
+)
+
+
+//Config Reading Example
+type Configuration struct {
+    BootstrapServer    []string
+}
+
+file, _ := os.Open("config.json")
+decoder := json.NewDecoder(file)
+configuration := Configuration{}
+err := decoder.Decode(&configuration)
+if err != nil {
+  fmt.Println("error:", err)
+}
+fmt.Println(configuration.BootstrapServer) // output: [BootstrapServer]
