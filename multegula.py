b3812b749ceccc5e1a33d274783d47628ceb645e
Garrett Miller <garrettmiller@gmail.com>
2016-03-23 16:21:43 -0400

    Added another necessary import to multegula.py

--- multegula.py
+++ multegula.py
@@ -1,16 +1,18 @@
 #!/usr/bin/python3
 ###########################################################
-#Multegula - multegula.py						          #
+#Multegula - multegula.py                                 #
 #Main Driver/Startup Script for Multegula                 #
 #Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He#
 ###########################################################
+
 #TODO: place multegula UI file in UI.py, add function "runUI" to run it.
 
 from UI import * #Import our UI functions
 import subprocess #Needed for system calls
+from multiprocessing import Process #Needed for function concurrency
 
 #Start MessagePasser
-startweb = subprocess.Popen(['go', 'run', 'MessagePasser.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+messagepasser = subprocess.Popen(['go', 'run', 'MessagePasser.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 
 #Start UI
 p1 = Process(target=runUI)
