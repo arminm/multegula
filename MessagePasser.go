52b727284e57082964e11c6254cc0f8894a4684b
Garrett Miller <garrettmiller@gmail.com>
2016-03-23 18:35:03 -0400

    Fixes to config reading example in MessagePasser.go

--- MessagePasser.go
+++ MessagePasser.go
@@ -4,23 +4,24 @@
 //Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He
 ////////////////////////////////////////////////////////////
 
-import (
-    "encoding/json"
-    "os"
-    "fmt"
-)
+package main
 
+import "encoding/json"
+import "os"
+import "fmt"
 
 //Config Reading Example
 type Configuration struct {
     BootstrapServer    []string
 }
 
-file, _ := os.Open("config.json")
-decoder := json.NewDecoder(file)
-configuration := Configuration{}
-err := decoder.Decode(&configuration)
-if err != nil {
-  fmt.Println("error:", err)
+func main(){
+	file, _ := os.Open("config.json")
+	decoder := json.NewDecoder(file)
+	configuration := Configuration{}
+	err := decoder.Decode(&configuration)
+	if err != nil {
+	  fmt.Println("error:", err)
+	}
+	fmt.Println(configuration.BootstrapServer) // output: [BootstrapServer]
 }
-fmt.Println(configuration.BootstrapServer) // output: [BootstrapServer]
