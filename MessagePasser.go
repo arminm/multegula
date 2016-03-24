fab9d2a0bc257ecc6039617e6bcb3c6ff3985670
helunwencser <lunwenh@andrew.cmu.edu>
2016-03-24 14:08:49 -0700

    define functions for setting up tcp connections

--- MessagePasser.go
+++ MessagePasser.go
@@ -10,8 +10,9 @@ import (
 	"encoding/json"
 	"os"
 	"fmt"
-	"net"
-	"bufio"
+//	"net"
+//	"bufio"
+	"sort"
 )
 
 //Config Reading Example
@@ -19,6 +20,40 @@ type Configuration struct {
     BootstrapServer  []string
     LocalName []string
     Group []string
+}
+
+/* map stores connections to each node
+ * <key, value> = <dns, connection>
+ **/
+var connections map[string]Conn = make(map[string]Conn)
+
+/* port number for TCP connection */
+const port string = ":8081"
+
+/* 
+ * accepts connections from other nodes and stores 
+ * connections into connections map
+ * @param	group
+ *			the DNS name of each node in the group
+ * 
+ * @param	localName
+ *			the DNS name of local node
+ **/
+func acceptConnection(group []string, sring localName) {
+	
+}
+
+/*
+ * send connections to nodes with greater DNS names
+ * and stores connections into connections map
+ * @param	group
+ *			the DNS name of each node in the group
+ *
+ * @param	localName
+ *			the DNS name of local node
+ **/
+func sendConnection(group []string, string localName) {
+	
 }
 
 func main(){
@@ -29,13 +64,15 @@ func main(){
 	if err != nil {
 	  fmt.Println("error:", err)
 	}
-	fmt.Println(configuration.BootstrapServer) // output: [BootstrapServer]
-	fmt.Println(configuration.LocalName)
-	fmt.Println(configuration.Group)
 
-	bootstrapServerDNS := configuration.BootstrapServer[0]
-	/* setup a connection to BootstrapServer and get the IPs of group */
-	conn, _ := net.Dial("tcp", bootstrapServerDNS);
-	
+	fmt.Println(configuration.BootstrapServer) // output: [BootstrapServer]
+	fmt.Println(configuration.LocalName) // output: [local.dyndns.org]
+	fmt.Println(configuration.Group) // output: [local.dyndns.org node1.dyndns.org node2.dyndns.org]
+
+	/* sort group's dns name, node only sends TCP connection to nodes
+	 * which are greater in the sorted order so that there is only one
+	 * TCP connection between each other
+	 **/
+	sort.Strings(configuration.Group)
 }
 
