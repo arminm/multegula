39670136ab26b528b12e89a2d037171a75b87825
helunwencser <lunwenh@andrew.cmu.edu>
2016-03-24 17:51:26 -0700

    finish most function for messagePasser, need test and refine

--- MessagePasser.go
+++ MessagePasser.go
@@ -12,6 +12,7 @@ import (
 	"fmt"
 	"net"
 	"bufio"
+	"strings"
 )
 
 //Config Reading Example
@@ -21,15 +22,59 @@ type Configuration struct {
     group []string
 }
 
-/* message */
-type Message struct {
+/* delimiter for formatting message */
+const delimiter string = "##"
 
+/* message structure
+ * before message transported through TCP connection, it will
+ * be converted to string in the format of: source##destination##content##kind
+ # when message is received, it will be reconstructed
+ **/
+type Message struct {
+	source string // the DNS name of sending node
+	destination string // the DNS name of receiving node
+	content string // the content of message
+	kind string //s the kind of messages
+}
+
+/* 
+ * convert message to string
+ * @param	message
+ *			message to be converted
+ *
+ * @return	the string format of the message
+ **/
+func encodeMessage(message Message) string {
+	return message.source + delimiter + message.destination + delimiter + message.content + delimiter + message.kind
+}
+
+/*
+ * construct message from it's string format
+ * @param	messageString
+ *			message in string format
+ *
+ * @return	message
+ **/
+func decodeMessage(messageString string) Message {
+	var elements []string = strings.Split(messageString, delimiter)
+	return Message{
+		source: elements[0],
+		destination: elements[1],
+		content: elements[2],
+		kind: elements[3]
+	}
 }
 
 /* map stores connections to each node
  * <key, value> = <dns, connection>
  **/
 var connections map[string]Conn = make(map[string]Conn)
+
+/* the queue for messages to be sent */
+var sendQueue chan = make(chan Message, 100)
+
+/* the queue for received messages */
+var receivedQueue chan = make(chan Message, 100)
 
 /* port number for TCP connection */
 const port string = ":8081"
@@ -103,7 +148,78 @@ func sendConnection(latterNodes map[string]bool, string localName) {
 	}
 }
 
-func main(){
+/* 
+ * receive message from TCP connection, reconstruct message and 
+ * put it into receivedQueue of message
+ * @param	conn
+ *			TCP connection
+ **/
+func receiveMessageFromConn(conn net.Conn) {
+	for {
+		messageString, _ := bufio.NewReader(conn).ReadString('\n')
+		if(len(messageString) > 0) {
+			receivedQueue <- decodeMessage(messageString)
+		}
+	}
+}
+
+/* 
+ * for each TCP connection, start a new receiveMessageFromConn routine to
+ * receive messages sent from that connection. A constraint for this mechanism
+ * is that each routine waits in a infinite loop which makes code inefficient.
+ * //TODO find more efficient way in go for listening messages for net.Conn
+ **/
+func startReceiveRoutine() {
+	for dns, conn := range connections {
+		go receiveMessageFromConn(conn)
+	}
+}
+
+/*
+ * whenever there are messages in sendQueue, send it out to TCP connection
+ **/
+func sendMessageToConn() {
+	for {
+		message := <- sendQueue
+		conn := connections[message.destination]
+		conn.Write([]byte(encodeMessage(message) + "\n"))
+	}
+}
+
+/* 
+ * put message to sendQueue, since the chan <- maybe blocked if the channel is full,
+ * in order to not block the void send(message Message) method, we creates a new routine
+ * to do this
+ * @param	message
+ *			the message to be put into sendQueue
+ **/
+func putMessageToSendQueue(message Message) {
+	sendQueue <- message
+}
+
+/* 
+ * send message, this is a public method
+ * @param	message
+ *			message to be sent
+ **/
+func Send(message Message) {
+	go putMessageToSendQueue(message)
+}
+
+/* 
+ * receive message, this is a public method
+ * @return	if there are receivable messages in receivedQueue, return the first
+ *			in receivedQueue; otherwise, return nil
+ **/
+func Receive() Message {
+	var message Message = nil
+	if(len(receivedQueue) > 0){
+		message = <- receivedQueue
+	}
+	return message
+}
+
+func main() {
 	file, _ := os.Open("config.json")
 	decoder := json.NewDecoder(file)
 	configuration := Configuration{}
@@ -112,15 +228,16 @@ func main(){
 	  fmt.Println("error:", err)
 	}
 
-	fmt.Println(configuration.bootstrapServer) // output: [BootstrapServer]
-	fmt.Println(configuration.localName) // output: [local.dyndns.org]
-	fmt.Println(configuration.group) // output: [local.dyndns.org node1.dyndns.org node2.dyndns.org]
-
+	/* separate DNS names */
 	frontNodes, latterNodes := getFrontAndLatterNodes(configuration.group, configuration.localName[0])
-	fmt.Println(frontNodes)
-	fmt.Println(latterNodes)
 
+	/* setup TCP connections */
 	acceptConnection(frontNodes)
 	sendConnection(latterNodes, configuration.localName[0])
-}
 
+	/* start routines listening on each connection to receive messages */
+	startReceiveRoutine()
+
+  	/* start routine to send message */
+  	go sendMessageToConn()
+}
