131153fb5c7fbbd27272614d6df4ae1f3b288988
Garrett Miller <garrettmiller@gmail.com>
2016-03-27 19:14:58 -0400

    Tiny bit of code for PyBridge.go

--- Bridges/PyBridge.go
+++ Bridges/PyBridge.go
@@ -11,3 +11,16 @@ import (
 	"fmt"
 	"net"
 )
+
+/*
+ * accepts connections from Python Bridge
+ * connections into connections map, after accepting
+ * all connections from all other nodes in the group,
+ * this routine exits
+
+ * @param	frontNodes
+ *			map that contains all nodes with smaller DNS names
+ **/
+func runPythonBridge() {
+    ln, _ := net.Listen("tcp", 44444)
+	conn, _ := ln.Accept()
