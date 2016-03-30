4222000f34ece2d4deb087b13c0f080353de343c
Garrett Miller <garrettmiller@gmail.com>
2016-03-30 13:16:04 -0400

    Changed package name on Bootstrap Server

--- BootstrapServer/BootstrapServer.go
+++ BootstrapServer/BootstrapServer.go
@@ -4,7 +4,7 @@
 //Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He
 ////////////////////////////////////////////////////////////
 
-package main
+package BootstrapServer
 
 import (
     "fmt"
