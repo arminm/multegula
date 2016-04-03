55841908b37ec6ee8ffdacf8a2b93e5c1515c4e8
Garrett Miller <garrettmiller@gmail.com>
2016-04-03 15:56:51 -0700

    Committing successful network test state.

--- Bridges/GoBridge.py
+++ Bridges/GoBridge.py
@@ -7,12 +7,15 @@
 import socket
 import time #Needed for labeling date/time
 import datetime #Needed for labeling date/time
+import base64 #Needed for encoding network messages
 
+########PARAMETERS########
 #Set arbitrary buffer size for received messages
 BUFFER_SIZE = 200
 DELIMITER = "##"
 LOCALHOST_IP = '127.0.0.1'
 TCP_PORT = 44444
+##########################
 
 
 ## RUN the GoBridge
@@ -29,18 +32,24 @@ def runGoBridge():
 		GoBridge.connect((LOCALHOST_IP, TCP_PORT))
 	except:
 		print("[" + prettytime + "] Can't connect. Is GoBridge up?")
+
+	###############FOR TESTING/DEBUG ONLY#################
+	message = "localhost##localhost##messagecontent##testMessage"
+	GoBridge.send(message.encode(encoding='utf-8'))
+	###############FOR TESTING/DEBUG ONLY#################
 	
 	#Close the connection, but commented because we don't want to do that yet.
 	#MessagePasser.close()
 
 ## Build and Send Message
-## # this function builds and sends a message
+## # this function builds and sends a message.
+## # Base64 encoding isn't my favorite, but it became necessary in Python 3.
 def sendMessage(src, dest, content, kind, multicastFlag):
 	message = src + DELIMITER + dest + DELIMITER + content + DELIMITER + kind + DELIMITER + multicastFlag
-	MessagePasser.send(message)
+	GoBridge.send(message.encode(encoding='utf-8'))
 
 ## Receive Message
 ## # this function receives a message from the receive buffer
 def receiveMessage():
-	receivedData = MessagePasser.recv(BUFFER_SIZE)
+	receivedData = GoBridge.recv(BUFFER_SIZE)
 	return receivedData
