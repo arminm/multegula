67d899fb81d6f0bc6058d0f5ea31814238c49710
Garrett Miller <garrettmiller@gmail.com>
2016-04-03 14:18:55 -0700

    Now using subprocess.check_output to get status code

--- multegula.py
+++ multegula.py
@@ -22,7 +22,8 @@ from Bridges.GoBridge import * #Import our Go Bridge
 #####################
 
 #Start Go Bridge, and give it time to come up before continuing
-messagepasser = subprocess.Popen(['go', 'run', 'Bridges/PyBridge.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+#messagepasser = subprocess.Popen(['go', 'run', 'Bridges/PyBridge.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+subprocess.check_output("go run Bridges/PyBridge.go", shell=True)
 time.sleep(3)
 
 #Start UI
