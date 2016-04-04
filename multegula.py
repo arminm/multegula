60ad4b3b9643c404a08bd36fa5afe25c198aee1f
Garrett Miller <garrettmiller@gmail.com>
2016-04-04 13:02:13 -0400

    Fixed master. Builds and runs just fine once again.

--- multegula.py
+++ multegula.py
@@ -8,20 +8,16 @@
 #######IMPORTS#######
 import sys #Needs to be here before the path append.
 import subprocess #Needed for system calls
-from multiprocessing import Process #Needed for function concurrency
 import time #Needed to sleep
 #####################
 
 #Tells Python to search UI folder for functions as well.
 sys.path.append('UI/')
-
-#####OUR IMPORTS#####
 from UI.multegulaUI import * #Import our UI functions
-######################Start PyBridge, and give it time to come up before continuing
+
+#Start PyBridge, and give it time to come up before continuing
 PyBridge = subprocess.Popen(['go', 'run', 'Bridges/PyBridge.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 time.sleep(1)
 
 #Start UI
-p1 = Process(target=runUI)
-p1.start()
-p1.join()
+runUI()
