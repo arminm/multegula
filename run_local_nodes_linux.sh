4b2128d5576d93a28bdcc6f30c15b020a72bc76d
Garrett Miller <garrettmiller@gmail.com>
2016-04-04 21:52:39 -0700

    actually adding Linux script this time

---
+++ run_local_nodes_linux.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+NODES=(armin lunwen daniel garrett)
+
+##############################
+# Modify these parameters:
+PWD=~/go/src/github.com/arminm/multegula
+CONFIG_FILE_NAME=~/go/src/github.com/arminm/multegula/messagePasser/localConfig.json
+
+##############################
+# generate launcher files
+for i in "${NODES[@]}"; do   
+    echo \#\!/bin/bash > $i.sh
+    echo cd $PWD >> $i.sh
+    echo go run multegula.go $CONFIG_FILE_NAME $i >> $i.sh
+    chmod +rwx $i.sh
+done
+
+##############################
+# Run launcher files
+
+for i in "${NODES[@]}"; do
+    echo launching $i
+    x-terminal-emulator -e bash $i.sh
+done
+
+sleep 1
+##############################
+# Clean launcher files (may not be necessary)
+
+for i in "${NODES[@]}"; do   
+    echo removing $i
+    rm $i.sh
+done
