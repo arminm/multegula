7915daf71e0fd61e42e988263853e7a989642f20
Armin Mahmoudi <arminm89@gmail.com>
2016-04-05 14:35:31 -0700

    Added Pop, Delete, and Insert

--- messagePasser/queue.go
+++ messagePasser/queue.go
@@ -1,13 +1,23 @@
 package messagePasser
 
-func Push(queue *[]Message, obj Message) {
-	*queue = append(*queue, obj)
+func Push(queue *[]Message, message Message) {
+	*queue = append(*queue, message)
 }
 
-func Pop(queue *[]Message, obj Message) Message {
-	return obj
+func Pop(queue *[]Message) Message {
+	message := (*queue)[0]
+	*queue = (*queue)[1:]
+	return message
 }
 
-func Insert(queue []Message, obj Message, index int) {
+func Delete(queue *[]Message, index int) {
+	*queue = append((*queue)[:index], (*queue)[index+1:]...)
+}
 
+func Insert(queue *[]Message, message Message, index int) *[]Message {
+	newQueue := make([]Message, len(*queue)+1)
+	copy(newQueue[:index], (*queue)[:index])
+	copy(newQueue[index+1:], (*queue)[index:])
+	newQueue[index] = message
+	return &newQueue
 }
--- messagePasser/queue_test.go
+++ messagePasser/queue_test.go
@@ -20,3 +20,42 @@ func TestPush(t *testing.T) {
 		t.Errorf("Message was not pushed to queue.\nQueue:%+v\nMessage:%v", queue, msg2)
 	}
 }
+
+func TestPop(t *testing.T) {
+	msg0 := Message{"Lunwen", "Armin", "Hi Armin!", "Regular", 1}
+	msg1 := Message{"Armin", "Lunwen", "Hi Lunwen!", "Regular", 1}
+	msg2 := Message{"Daniel", "", "Hi All!", "Multicast", 1}
+	queue := make([]Message, 3, 5)
+	queue[0], queue[1], queue[2] = msg0, msg1, msg2
+
+	result := Pop(&queue)
+	if result != msg0 {
+		t.Errorf("Message was not popped.\nQueue:%+v\nResult:%+v", queue, result)
+	}
+}
+
+func TestDelete(t *testing.T) {
+	msg0 := Message{"Lunwen", "Armin", "Hi Armin!", "Regular", 1}
+	msg1 := Message{"Armin", "Lunwen", "Hi Lunwen!", "Regular", 1}
+	msg2 := Message{"Daniel", "", "Hi All!", "Multicast", 1}
+	queue := make([]Message, 3, 5)
+	queue[0], queue[1], queue[2] = msg0, msg1, msg2
+
+	Delete(&queue, 1)
+	if queue[1] == msg1 {
+		t.Errorf("Message was not deleted.\nQueue:%+v\nMessage:%+v", queue, msg1)
+	}
+}
+
+func TestInsert(t *testing.T) {
+	msg0 := Message{"Lunwen", "Armin", "Hi Armin!", "Regular", 1}
+	msg1 := Message{"Armin", "Lunwen", "Hi Lunwen!", "Regular", 1}
+	msg2 := Message{"Daniel", "", "Hi All!", "Multicast", 1}
+	queue := make([]Message, 3, 5)
+	queue[0], queue[1] = msg0, msg1
+
+	queue = *(Insert(&queue, msg2, 1))
+	if queue[1] != msg2 {
+		t.Errorf("Message was not inserted.\nQueue:%+v\nMessage:%+v", queue, msg2)
+	}
+}
