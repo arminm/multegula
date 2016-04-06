10f06dbc6cf8afd2eacde4ec9f3f93eacdb19044
Garrett Miller <garrettmiller@gmail.com>
2016-04-06 14:25:03 -0700

    Updated a path in multegula.py

--- multegula.py
+++ multegula.py
@@ -16,7 +16,7 @@ sys.path.append('UI/')
 from UI.multegulaUI import * #Import our UI functions
 
 #Start PyBridge, and give it time to come up before continuing
-PyBridge = subprocess.Popen(['go', 'run', 'Bridges/PyBridge.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+PyBridge = subprocess.Popen(['go', 'run', 'bridges/PyBridge.go'], stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
 time.sleep(1)
 
 #Start UI
