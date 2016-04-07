09181bb959b1887bcc3655dda1692ac62d39e42a
Garrett Miller <garrettmiller@gmail.com>
2016-04-07 16:49:53 -0700

    Update Windows runscript.

--- run.bat
+++ run.bat
@@ -1,3 +1,8 @@
 @echo off
-python multegula.py %*
+call "cmd /c go run multegula.go config bob"
+
+rem de-facto sleep.  Ping an address that doesn't exist and wait 2000ms.
+ping 192.0.2.2 -n 1 -w 2000 > nul
+
+call "cmd /c python multegula.py %*"
 pause
