21ed735e100d8d1688412424a24cd76a4ba6dca4
Armin Mahmoudi <arminm89@gmail.com>
2016-04-09 09:56:15 -0700

    Using DeepEqual() to compare messages

--- bridges/PyBridge.go
+++ bridges/PyBridge.go
@@ -1,5 +1,5 @@
 ////////////////////////////////////////////////////////////
-//Multegula - PyBridge.go 
+//Multegula - PyBridge.go
 //Server for interacting with UI written in Python
 //Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He
 ////////////////////////////////////////////////////////////
@@ -7,11 +7,13 @@
 package bridges
 
 import (
-    "fmt"
-    "net"
-    "bufio"
-    "github.com/arminm/multegula/messagePasser"
-    "strings"
+	"bufio"
+	"fmt"
+	"net"
+	"reflect"
+	"strings"
+
+	"github.com/arminm/multegula/messagePasser"
 )
 
 /* port number for local TCP connection */
@@ -32,7 +34,7 @@ func decodeMessage(messageString string) messagePasser.Message {
 	return messagePasser.Message{Source: elements[0], Destination: elements[1], Content: elements[2], Kind: elements[3]}
 }
 
-/* 
+/*
  * convert message to string
  * @param	message
  *			message to be converted
@@ -49,13 +51,13 @@ func encodeMessage(message messagePasser.Message) string {
  *        local connection for interacting with UI
  **/
 func receiveFromUI(conn net.Conn) {
-    for {
-        messageString, _ := bufio.NewReader(conn).ReadString('\n')
-        if(len(messageString) > 0) {
-            fmt.Printf("Message received from UI: %s\n", messageString[0:len(messageString) - 1])
-            messagePasser.Send(decodeMessage(messageString[0:len(messageString) - 1]))
-        }
-    }
+	for {
+		messageString, _ := bufio.NewReader(conn).ReadString('\n')
+		if len(messageString) > 0 {
+			fmt.Printf("Message received from UI: %s\n", messageString[0:len(messageString)-1])
+			messagePasser.Send(decodeMessage(messageString[0 : len(messageString)-1]))
+		}
+	}
 }
 
 /**
@@ -64,27 +66,27 @@ func receiveFromUI(conn net.Conn) {
  *        local connection for interacting with UI
  **/
 func sendToUI(conn net.Conn) {
-    for {
-        var message messagePasser.Message = messagePasser.BlockReceive()
-        if(message != messagePasser.Message{}) {
-            fmt.Printf("Message sent to UI: %s\n", encodeMessage(message))
-            conn.Write([]byte(encodeMessage(message) + "\n"))
-        }
-    }
+	for {
+		var message messagePasser.Message = messagePasser.BlockReceive()
+		if !reflect.DeepEqual(message, messagePasser.Message{}) {
+			fmt.Printf("Message sent to UI: %s\n", encodeMessage(message))
+			conn.Write([]byte(encodeMessage(message) + "\n"))
+		}
+	}
 }
 
 func InitPyBridge(configFile string, localName string) {
-    messagePasser.InitMessagePasser(configFile, localName)
+	messagePasser.InitMessagePasser(configFile, localName)
 
-    ln, err := net.Listen("tcp", port)
-    if(err != nil) {
-        fmt.Println(err)
-    }
+	ln, err := net.Listen("tcp", port)
+	if err != nil {
+		fmt.Println(err)
+	}
 
-    conn, _ := ln.Accept()
+	conn, _ := ln.Accept()
 
-    /* start a new routine to receive messages from UI */
-    go receiveFromUI(conn)
+	/* start a new routine to receive messages from UI */
+	go receiveFromUI(conn)
 
-    sendToUI(conn)
+	sendToUI(conn)
 }
