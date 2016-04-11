819f53734e189dfb0c95080fab05b7666cfec0e2
Garrett Miller <garrettmiller@gmail.com>
2016-04-10 18:27:27 -0700

    Removing unnecessary sleep in runscript

--- run.sh
+++ run.sh
@@ -11,12 +11,11 @@ killall multegula 2> /dev/null
 sleep 1
 
 bash -c 'go run multegula.go config bob' &
-sleep 2
+sleep 3
 
 #Start UI only if background "go run" job succeeded. Works by checking if we have a process ID.
 if kill -0 $!; then
     echo "Multegula core started successfully! Starting UI."
-    sleep 2
     python3 UI/multegulaUI.py
 else
     echo "Multegula core failed to start!  Aborting UI."
