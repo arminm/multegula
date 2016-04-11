0ece93bdbb77ef52eabbf5721638651c42ae703e
Garrett Miller <garrettmiller@gmail.com>
2016-04-11 11:29:56 -0700

    Initial draft of run_bootstrap.sh

---
+++ run_bootstrap.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+###########################################################
+#Multegula - run_bootstrap.sh                             #
+#Startup Script for Multegula Bootstrap Server            #
+#Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He#
+###########################################################
+
+#Cleanup any running go
+killall go 2> /dev/null
+killall multegula 2> /dev/null
+sleep 1
+
+#Start multegula.go and wait for it to come up.
+#Defaults to port 55555 if port isn't received.
+go run bootstrapServer/BootstrapServer.go -port=${1:-55555} &
