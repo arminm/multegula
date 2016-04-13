3e6ba5a77143006cf5c491f71b318143f07c85b9
Armin Mahmoudi <arminm89@gmail.com>
2016-04-12 21:46:02 -0700

    added extra condition

--- messagePasser/MessagePasser.go
+++ messagePasser/MessagePasser.go
@@ -142,7 +142,9 @@ func updateSeqNum(message *Message) {
 func isMessageReady(message Message, sourceIndex int, localTimeStamp *[]int) bool {
 	for i, val := range message.Timestamp {
 		localValue := (*localTimeStamp)[i]
-		if i == sourceIndex && val != (localReceivedSeqNum+1) {
+		if i == localIndex && val != (localReceivedSeqNum+1) {
+			return false
+		} else if i == sourceIndex && val != (localValue+1) {
 			return false
 		} else if i != sourceIndex && val > localValue {
 			return false
