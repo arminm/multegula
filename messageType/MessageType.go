e4b7ebc8cc3132a4f2f2ec0a41623657ae73896e
helunwencser <lunwenh@andrew.cmu.edu>
2016-04-13 10:28:05 -0700

    add message type define

---
+++ messageType/MessageType.go
@@ -0,0 +1,19 @@
+////////////////////////////////////////////////////////////
+//Multegula - MessageType.go
+//Multicasting Message Passer for Multegula
+//Armin Mahmoudi, Daniel Santoro, Garrett Miller, Lunwen He
+////////////////////////////////////////////////////////////
+
+////////////////////////////////////////////////////////////
+// This file defines all message types used in this project
+////////////////////////////////////////////////////////////
+
+package messageType
+
+/* The kinds of message will be used in bully algorithm */
+/* The election message */
+const ELECTION string = "election"
+/* The answer message */
+const ANSWER string = "answer"
+/* The coordinator message */
+const COORDINATOR string = "coordinator"
