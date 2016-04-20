4127c7acb5ad80d456e73e3a54adfddb272e2dd2
Garrett Miller <garrettmiller@gmail.com>
2016-04-20 12:56:52 -0400

    Reducing number of level files to three.

--- UI/levels/level05.py
+++
@@ -1,4 +0,0 @@
-# level05 PLACEHOLDER - horizontal blocks
-for x in range(1, 4) :
-	for y in range(Y_THIRD, Y_2THIRD, BLOCK_HEIGHT*4) :
-		thisLevel.append(Block(x*X_THIRD, y, PowerUps.PWR_NONE, Tilt.HORZ))
