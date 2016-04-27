c6c5901701a05e2f2c9741cfb29e414f25103810
dansantorojr <daniel.d.santoro@gmail.com>
2016-04-26 21:25:58 -0400

    Tiny updates.

--- multegula.go
+++ multegula.go
@@ -268,7 +268,6 @@ func inboundDispatcher() {
 	for {
 		// get message from MessagePasser
 		message := messagePasser.Receive()
-
 		// Based on the type of message, determine where it needs routed
 		switch message.Kind {
 
@@ -474,7 +473,15 @@ func main() {
 
 		// initialize elections
 		go bullySelection.InitBullySelection(*peers, localNodeName)
-		go UnicornReciever()
+		//go UnicornReciever()
+		/***** TODO: REPLACE WITH ACTUAL ELECTION INFORMATION *******/
+		// NOTE: This will happen somewhere else
+		var unicornMsg messagePasser.Message;
+		unicornMsg.Source = localNode.Name;
+		unicornMsg.Destination = defs.MULTICAST_DEST;
+		unicornMsg.Kind = defs.MSG_UNICORN;
+		unicornMsg.Content = "a";
+		bridges.SendToPyBridge(unicornMsg)
 
 		/* start the routine waiting for messages coming from UI */
 		go PyBridgeReceiver()
